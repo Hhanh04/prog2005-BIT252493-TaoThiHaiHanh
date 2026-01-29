ddd76577eab4b32498946a57b3c8b2e37b3b1c93
Hhanh04 <bit252493@st.cmcu.edu.vn>
2026-01-29 22:29:30 +0700

    Create Ex3.py

---
+++ Ex3.py
@@ -0,0 +1,10 @@
+so_1 = int(input("Nhập số nguyên thứ nhất:"))
+so_2 = int(input("Nhập số nguyên thứ hai:"))
+tong = so_1 + so_2
+hieu = so_1 - so_2
+tich = so_1 * so_2
+thuong = so_1 // so_2
+print("Tổng của so_1 và so_2 là","Tổng", so_1, "và", so_2, "là", tong)
+print("Hiệu của so_1 và so_2 là","Hiệu", so_1, "và", so_2, "là", hieu)
+print("Tích của so_1 và so_2 là","Tích", so_1, "và", so_2, "là", tich)
+print("Thương của số 1 và số 2 là","Thương", so_1, "và", so_2, "là", thuong)
