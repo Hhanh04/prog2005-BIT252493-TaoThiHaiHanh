6431754b7d7a4e57285b09d5380ec582ff36af2b
Hhanh04 <bit252493@st.cmcu.edu.vn>
2026-02-05 15:11:07 +0700

    Create Ex1.py

---
+++ Chapter2/Ex1.py
@@ -0,0 +1,6 @@
+num = int(input("Nhập một số từ 1 đến 9: "))
+if 1 <= num <= 9:
+    for i in range(1, 10):
+        print(f"{num} x {i} = {num * i}")
+else:
+    print("Số nhập không hợp lệ. Vui lòng nhập từ 1 đến 9.")
