b45c1d01406fa88f63926793fd65325c76a643fb
Hhanh04 <bit252493@st.cmcu.edu.vn>
2026-02-05 15:21:56 +0700

    Create Ex13.py

---
+++ Chapter2/Ex13.py
@@ -0,0 +1,6 @@
+password = ""
+while password != "python123":
+    password = input("Nhập mật khẩu: ")
+    if password != "python123":
+        print("Mật khẩu sai. Thử lại.")
+print("Mật khẩu đúng!")
