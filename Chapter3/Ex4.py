8e6edad1eab4405d5096e6e9ec7f5a62768d1717
Hhanh04 <bit252493@st.cmcu.edu.vn>
2026-03-05 11:55:25 +0700

    Create Ex4.py

---
+++ Chapter3/Ex4.py
@@ -0,0 +1,7 @@
+so_nguyen = [5, 2, 9, 1, 5, 7]
+so_nguyen.sort()
+print("Sau khi sort():", so_nguyen)
+so_nguyen.reverse()
+print("Sau khi reverse():", so_nguyen)
+dem_so_5 = so_nguyen.count(5)
+print(f"Số lần xuất hiện của số 5: {dem_so_5}")
