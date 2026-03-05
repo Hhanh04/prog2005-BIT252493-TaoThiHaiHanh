38c5ca723f2058721d5d6d7db16472b1bdfb8cc8
Hhanh04 <bit252493@st.cmcu.edu.vn>
2026-03-05 11:52:27 +0700

    Create Ex2.py

---
+++ Chapter3/Ex2.py
@@ -0,0 +1,6 @@
+ban_be = ["An", "Binh", "Cang"]
+print("Danh sách ban đầu:", ban_be)
+ban_be.append("Dung")
+print("Sau khi thêm Dung:", ban_be)
+ban_be.pop(1)
+print("Sau khi xóa bạn thứ 2:", ban_be)
