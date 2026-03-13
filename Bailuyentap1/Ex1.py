62f20a05fb73f9fad4bbe2368b7cfb04521f48b0
Hhanh04 <bit252493@st.cmcu.edu.vn>
2026-03-13 17:30:26 +0700

    Create Ex1.py

---
+++ Bailuyentap1/Ex1.py
@@ -0,0 +1,6 @@
+def bai1():
+    """Bài 1: Tính (a² + b²)/(a-b) với a=10, b=4"""
+    a, b = 10, 4
+    result = (a ** 2 + b ** 2) / (a - b)
+    print(f"BÀI 1: (10² + 4²)/(10-4) = {result}")
+    print("-" * 50)
