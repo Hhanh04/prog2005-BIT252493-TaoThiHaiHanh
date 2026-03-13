e26248350aefd165b6ac9797ea33a5649afcbd8a
Hhanh04 <bit252493@st.cmcu.edu.vn>
2026-03-13 17:33:29 +0700

    Create Ex6.py

---
+++ Bailuyentap1/Ex6.py
@@ -0,0 +1,8 @@
+def is_prime(num):
+    """Kiểm tra số nguyên tố"""
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
